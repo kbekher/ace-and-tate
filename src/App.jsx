b34b1ce5d5a822afd7104b3c66bfe0ee16a178eb
Kristina Bekher <bekherkr@gmail.com>
2024-03-11 20:58:25 +0100

    initial commmit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+export const App = () => {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="App__content">
+          <h1>Ace & Tate</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
