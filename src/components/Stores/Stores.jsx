876aa3c6c50b2329025c9cb0cf41d8f2529e056e
Kristina Bekher <bekherkr@gmail.com>
2024-03-16 14:09:28 +0100

    update

---
+++ src/components/Stores/Stores.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Stores.scss';
+
+export const Stores = () => {
+  return (
+    <div className='Stores'>
+      <div className="Stores__content"></div>
+    </div>
+  );
+}
+
