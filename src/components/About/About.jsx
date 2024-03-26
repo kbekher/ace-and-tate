283a1a83dc240dd31d961a5af5c15914a175c8e0
Kristina Bekher <bekherkr@gmail.com>
2024-03-26 21:06:34 +0100

    update images

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,9 +6,9 @@ export const About = () => {
   return (
     <div className="About">
       <div className="About__content">
-        <img src="./assets/design/collab1.webp" alt="" className="About__img1"/>
-        <img src="./assets/design/collab2.webp" alt="" className="About__img2"/>
-        <img src="./assets/design/collab3.webp" alt="" className="About__img3"/>
+        <img src="./assets/design/collab1.jpg" alt="" className="About__img1"/>
+        <img src="./assets/design/collab2.jpg" alt="" className="About__img2"/>
+        <img src="./assets/design/collab3.jpg" alt="" className="About__img3"/>
 
         <h3>Our GANNI collab is back</h3>
       </div>
