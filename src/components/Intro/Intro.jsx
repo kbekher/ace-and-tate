b3fe037451bf6f82e01e426272a8918f339910a0
Kristina Bekher <bekherkr@gmail.com>
2024-03-26 21:04:12 +0100

    update header images

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,7 +8,7 @@ export const Intro = () => {
       <div className="Intro__content">
         <div className="Intro__textBox">
           <h1 className='Intro__title'>Get to know our glasses</h1>
-          <img src="./assets/design/header-el.webp" alt="header element" className="Intro__innerImg" />
+          <img src="./assets/design/header-el.jpg" alt="header element" className="Intro__innerImg" />
         </div>
       </div>
     </div>
